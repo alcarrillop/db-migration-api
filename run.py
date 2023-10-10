034fae7dba56519b958e7987564e7762439dd5a6
Andres Carrillo <alcarrillop@gmail.com>
2023-10-10 10:24:27 -0500

    rebase files to run

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from flask_migrate import Migrate
 from api import app, db
-
-migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
