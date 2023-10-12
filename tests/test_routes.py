ed8002d444f04e07973d65d4cd6fb6f37d7b1d82
Andres Carrillo <alcarrillop@gmail.com>
2023-10-12 18:06:46 -0500

    Data for testing

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -16,12 +16,10 @@ def test_client():
 def test_hires_by_quarter(test_client):
     response = test_client.get('/hires-by-quarter')
     assert response.status_code == 200
-    # Add more assertions based on expected data
 
 def test_departments_above_average(test_client):
     response = test_client.get('/departments-above-average')
     assert response.status_code == 200
-    # Add more assertions based on expected data
 
 def test_upload_csv(test_client):
     data = {
@@ -29,19 +27,12 @@ def test_upload_csv(test_client):
     }
     response = test_client.post('/upload-csv', content_type='multipart/form-data', data=data)
     assert response.status_code == 200
-    # Add more assertions based on expected data
 
 def test_batch_insert(test_client):
-    data = [
-        {
-            "type": "employee",
-            "id": 1,
-            "name": "Test Employee",
-            "datename": "2021-01-01",
-            "department_id": 1,
-            "job_id": 1
-        }
-    ]
-    response = test_client.post('/batch-insert', data=json.dumps(data), content_type='application/json')
+    # Abre el archivo de prueba
+    with open('data/batch.json', 'rb') as data_file:
+        # Envía el archivo en la solicitud
+        response = test_client.post('/batch-insert', data={'file': data_file}, content_type='multipart/form-data')
+    
     assert response.status_code == 200
-    # Add more assertions based on expected data
+
